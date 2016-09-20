373453497f8d24b9ab2fe6fe8e1dabdfad84f39e
luisfelipekai <luisfelipekai@gmail.com>
2016-09-20 15:33:09 -0300

    Download images from url test

--- downloadImagesAPI/views.py
+++ downloadImagesAPI/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from bs4 import BeautifulSoup
+import urllib
+import os
+import urllib.request
 
 # Create your views here.
 @api_view(['GET'])
 def downloadImages(request):
+    # url = "http://www.deviantart.com/browse/all/?section=&global=1&q=dota"
+    # html = urllib.request.urlopen(url).read()
+    # sopa = BeautifulSoup(html, "html.parser")
+    # imgs = sopa.find_all("img", _class ="")
+    # count = 0
+    # for img in imgs:
+    #     count = count + 1
+    #     image_url = img['src']
+    #     urllib.request.urlretrieve(image_url, str(count) + ".jpg")
     return Response('')
